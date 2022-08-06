cdde1664b34442b756e666a088178e3e2bf0ec8b
veera tej <aradhyulaveerateja03@gmail.com>
2022-08-06 11:30:07 +0530

    transaction page initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionPageComponent } from './transaction-page/transaction-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionPageComponent
   ],
   imports: [
     BrowserModule,
